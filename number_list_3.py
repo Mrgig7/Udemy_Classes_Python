0333cc17de96e160347f4a0f3057b0a942a767a5
Mrgig7 <jnitesh1463@gmail.com>
2022-11-27 20:47:30 +0530

    Add files via upload

---
+++ number_list_3.py
@@ -0,0 +1,13 @@
+empty_list = []
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+numbers = [even, odd]
+print(numbers)
+
+for numbers_list_3 in numbers:
+    print(numbers_list_3)
+
+    for value in numbers_list_3:
+        print(value)
+
